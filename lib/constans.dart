7655243d05b552afd6aa602bdc45b4141424b00e
GyroPhan <khanhhung.dev@gmail.com>
2021-08-06 15:59:50 +0700

    update 6/8

--- lib/constans.dart
+++ lib/constans.dart
@@ -5,4 +5,48 @@ LinearGradient kBackgroundColor = LinearGradient(
     begin: Alignment.topRight,
     colors: [Color(0xffd1dded), Colors.white]);
 
+LinearGradient kTextPressColor = LinearGradient(
+    end: Alignment.bottomLeft,
+    begin: Alignment.topRight,
+    colors: [
+      Color(0xff8A2387),
+      Color(0xffE94057),
+      Color(0xffF27121),
+    ]);
+LinearGradient kTextDefaultColor = LinearGradient(
+    end: Alignment.bottomLeft,
+    begin: Alignment.topRight,
+    colors: [
+      Color(0xff4e54c8),
+      Color(0xff8f94fb),
+    ]);
+
 final Color baseColor = Color(0xFFF2F2F2);
+
+class GradientText extends StatelessWidget {
+  GradientText({
+    required this.text,
+    required this.gradient,
+  });
+
+  final String text;
+  final Gradient gradient;
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      shaderCallback: (bounds) => gradient.createShader(
+        Rect.fromLTWH(0, 0, bounds.width, bounds.height),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          // The color must be set to white for this to work
+          color: Colors.white,
+          fontSize: 50,
+          fontFamily: 'Gum',
+        ),
+      ),
+    );
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,33 +12,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
-  late AnimationController animationController;
-  late Animation<double> anim;
+  late AnimationController animation1Controller;
+  late AnimationController animation2Controller;
+
+  late Animation<double> anim1;
+  late Animation<double> anim2;
 
   double percent = 0;
-  static int timeMinute = 25;
-  int timeInSec = timeMinute * 60;
+  int minu = 5;
+
   late Timer timer;
   bool isPress = false;
+
   _startTimer() {
-    double secPercent = (timeInSec / 100);
+    print('Start timer ${minu}');
+    int sec = minu * 60;
+    double secPercent = (sec / 100);
     timer = Timer.periodic(Duration(seconds: 1), (timer) {
       setState(() {
-        if (timeInSec > 0) {
-          timeInSec--;
-          if (timeInSec % 60 == 0) {
-            timeMinute--;
+        isPress = true;
+        if (sec > 0) {
+          sec--;
+          if (sec % 60 == 0) {
+            minu--;
           }
-          if (timeInSec % secPercent == 0) {
-            if (percent < 1) {
+          if (sec % secPercent == 0) {
+            if (percent < 0.99) {
               percent += 0.01;
             } else {
-              percent = 1;
+              percent = 0.99;
             }
           }
         } else {
+          isPress = false;
           percent = 0;
-          timeMinute = 25;
+          minu = 5;
           timer.cancel();
         }
       });
@@ -49,35 +57,52 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   void initState() {
     // TODO: implement initState
     super.initState();
-    animationController =
+    //animation 1
+    animation1Controller =
         AnimationController(duration: Duration(seconds: 5), vsync: this);
     final curvedAnim = CurvedAnimation(
-        parent: animationController,
+        parent: animation1Controller,
         curve: Curves.bounceIn,
         reverseCurve: Curves.easeOut);
-    anim = Tween<double>(begin: 0, end: 2 * math.pi).animate(curvedAnim)
+    anim1 = Tween<double>(begin: 0, end: 2 * math.pi).animate(curvedAnim)
       ..addListener(() {
         setState(() {});
       })
       ..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
-          animationController.reverse();
+          animation1Controller.reverse();
         } else if (status == AnimationStatus.dismissed) {
-          animationController.forward();
+          animation1Controller.forward();
         }
       });
-    animationController.forward();
+    animation1Controller.forward();
+    //animation 2
+    animation2Controller =
+        AnimationController(duration: Duration(seconds: 5), vsync: this);
+    anim2 =
+        Tween<double>(begin: 0, end: 2 * math.pi).animate(animation2Controller)
+          ..addListener(() {
+            setState(() {});
+          })
+          ..addStatusListener((status) {
+            if (status == AnimationStatus.completed) {
+              animation2Controller.repeat();
+            }
+          });
+    animation2Controller.forward();
   }
 
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
-    animationController.dispose();
+    animation1Controller.dispose();
+    animation2Controller.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
     return Container(
       decoration: BoxDecoration(
         gradient: kBackgroundColor,
@@ -87,14 +112,12 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
         body: SingleChildScrollView(
           child: Container(
             child: SafeArea(
-              child: Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      height: 60,
-                    ),
-                    Stack(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    height: height - 200,
+                    child: Stack(
                       alignment: Alignment.center,
                       children: [
                         ClayContainer(
@@ -112,18 +135,12 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                           animationDuration: 2500,
                           percent: percent, // percent use
                           animateFromLastPercent: true,
-                          center: Text(
-                            "HUNG",
-                          ),
 
                           circularStrokeCap: CircularStrokeCap.round,
-
-                          widgetIndicator: RotatedBox(
-                            quarterTurns: 1,
-                            child: Icon(
-                              Icons.airplanemode_active_outlined,
-                              size: 20,
-                              color: Colors.white,
+                          widgetIndicator: Transform.rotate(
+                            angle: isPress ? anim2.value : 0,
+                            child: Container(
+                              child: Image.asset('assets/bone.png'),
                             ),
                           ),
                           maskFilter: MaskFilter.blur(BlurStyle.solid, 3),
@@ -139,70 +156,73 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                           width: 180,
                           borderRadius: 300,
                           emboss: false,
-                          curveType: CurveType.concave,
+                          curveType:
+                              isPress ? CurveType.concave : CurveType.convex,
                         ),
-                        Transform.rotate(
-                          angle: anim.value,
-                          child: Container(
-                            height: 100,
-                            child: Image.asset('assets/pug_icon.png'),
+                        GestureDetector(
+                          onTap: _startTimer,
+                          child: Transform.rotate(
+                            angle: isPress ? anim1.value : 0,
+                            child: Container(
+                              height: 100,
+                              child: Image.asset('assets/pug_icon.png'),
+                            ),
                           ),
                         ),
                       ],
                     ),
-                    SizedBox(height: 40),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        ClayContainer(
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            minu = minu - 5;
+                            print(minu);
+                          });
+                        },
+                        child: ClayContainer(
                           color: baseColor,
-                          height: 50,
-                          width: 50,
-                          borderRadius: 75,
-                          curveType: CurveType.concave,
-                        ),
-                        SizedBox(width: 50),
-                        ClayContainer(
-                          color: baseColor,
-                          height: 50,
-                          width: 50,
-                          borderRadius: 75,
-                          curveType: CurveType.none,
-                        ),
-                        SizedBox(width: 50),
-                        ClayContainer(
-                          color: baseColor,
-                          height: 50,
-                          width: 50,
+                          height: 60,
+                          width: 60,
                           borderRadius: 75,
                           curveType: CurveType.convex,
-                        ),
-                      ],
-                    ),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    GestureDetector(
-                      onTap: _startTimer,
-                      child: ClayContainer(
-                        color: baseColor,
-                        height: 50,
-                        width: 300,
-                        borderRadius: 75,
-                        curveType: CurveType.convex,
-                        child: Center(
-                          child: Text(
-                            'START',
-                            style: TextStyle(
-                                fontSize: 40,
-                                fontFamily: 'Gum',
-                                color: Colors.blueAccent),
+                          child: Icon(
+                            Icons.arrow_left_outlined,
+                            size: 50,
+                            color: Colors.redAccent,
                           ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
+                      GradientText(
+                          text: minu.toString(), gradient: kTextPressColor),
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            minu = minu + 5;
+                            print(minu);
+                          });
+                        },
+                        child: ClayContainer(
+                          color: baseColor,
+                          height: 60,
+                          width: 60,
+                          borderRadius: 75,
+                          curveType: CurveType.convex,
+                          child: Icon(
+                            Icons.arrow_right,
+                            size: 50,
+                            color: Colors.redAccent,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                ],
               ),
             ),
           ),
